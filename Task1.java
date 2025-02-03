b4b9d53364c0a96d2291b2118ddc5a218b615432
Vladiradi <rzhvlad@gmail.com>
2025-02-03 21:53:23 +0100

    2/4 task

---
+++ Task1.java
@@ -0,0 +1,36 @@
+public class Task1 {
+    public void execute() {
+        System.out.println("\nTask 1: Примитивные и ссылочные типы");
+
+        printPrimitiveTypes();
+        printReferenceTypes();
+    }
+
+    private void printPrimitiveTypes() {
+        char charValue = 'G';
+        int intValue = 89;
+        byte byteValue = 4;
+        short shortValue = 56;
+        float floatValue = 4.7333436f;
+        double doubleValue = 4.355453532;
+        long longValue = 12121L;
+
+        System.out.println("Примитивные типы:");
+        System.out.printf("char: %c, int: %d, byte: %d, short: %d, float: %.7f, double: %.9f, long: %d%n",
+                charValue, intValue, byteValue, shortValue, floatValue, doubleValue, longValue);
+    }
+
+    private void printReferenceTypes() {
+        Character charObject = 'G';
+        Integer intObject = 89;
+        Byte byteObject = 4;
+        Short shortObject = 56;
+        Float floatObject = 4.7333436f;
+        Double doubleObject = 4.355453532;
+        Long longObject = 12121L;
+
+        System.out.println("\nСсылочные типы:");
+        System.out.printf("Character: %c, Integer: %d, Byte: %d, Short: %d, Float: %.7f, Double: %.9f, Long: %d%n",
+                charObject, intObject, byteObject, shortObject, floatObject, doubleObject, longObject);
+    }
+}
---
+++ Task2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public void execute() {
+        System.out.println("\nTask 2: Разбиение числа");
+
+        int number = getInputNumber();
+        if (isValidThreeDigitNumber(number)) {
+            printDigits(number);
+        } else {
+            System.out.println("Введите трехзначное число!");
+        }
+    }
+
+    private int getInputNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите трехзначное число: ");
+        return scanner.nextInt();
+    }
+
+    private boolean isValidThreeDigitNumber(int number) {
+        return number >= 100 && number <= 999;
+    }
+
+    private void printDigits(int number) {
+        int hundreds = number / 100;
+        int tens = (number / 10) % 10;
+        int units = number % 10;
+
+        System.out.printf("Число %d -> %d, %d, %d%n", number, hundreds, tens, units);
+    }
+}
