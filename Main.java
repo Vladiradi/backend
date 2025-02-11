7fb059093601ec3eededd6fe34b9cd2bbed8c8d2
Vladiradi <rzhvlad@gmail.com>
2025-02-11 12:17:16 +0100

    task solved

--- Main.java
+++ Main.java
@@ -1,52 +1,38 @@
-//вводим цену машины
-//вводим имеющуюся на руках сумму
-// вводим сумму, которую мы можем ежемесячно откладывать
-// вводим boolean переменную, есть ли у вас кредитная история(то есть есть ли возможность взять заем в банке)
-//если суммы на руках достаточно, то покупаем
-//если сумма на руках меньше, чем цена машины, то считаем количество месяцев, необходимое для накопления
-//если количество месяцев меньше 12, -> будем копить
-//если количество месяцев, необходимое для накопления больше 12,
-// то проверяем, если есть возможность взять кдедит в банке, и если такая возможность есть,
-// выводим на экран сумму которую возьмем в банке
-// (то есть не копим, а берем заем разницы между ценой и суммой на руках) и вывести строчку -> машина куплена!!!
+//Создайте switch-блок, который мог бы определить, является ли переданный день выходным или рабочим днем.
+//        Создайте переменные int day; String dayString;
+//        Реализуйте тот же процесс используя if-else блоки
+//        Реализуйте тот же процесс используя тернарное выражение
+//        Сравните решения
 
 package telran.lessons;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите цену машины: ");
-        double carPrice = scanner.nextDouble();
-        System.out.print("Введите сумму, которая у вас есть: ");
-        double myMoney = scanner.nextDouble();
-        System.out.print("Введите сумму, которую можете откладывать каждый месяц: ");
-        double monthlySavings = scanner.nextDouble();
-        System.out.print("Есть ли у вас кредитная история: ");
-        boolean hasCreditHistory = scanner.nextBoolean();
-
-        if (myMoney >= carPrice) {
-            System.out.println("Поздравляем! Вы можете купить машину сразу.");
-        } else {
-            double moneyNeeded = carPrice - myMoney;
-            int monthsToSave = (int) Math.ceil(moneyNeeded / monthlySavings);
-
-            System.out.println("Вам не хватает " + moneyNeeded + "Euro" +
-                    "");
-            System.out.println("При текущих накоплениях вам понадобится " + monthsToSave + " месяцев.");
-
-            if (monthsToSave <= 12) {
-                System.out.println("Вы можете накопить на машину в течение года. Копим!");
-            } else if (hasCreditHistory) {
-                System.out.println("Ожидание более 12 месяцев. Можно взять заем в банке.");
-                System.out.println("Сумма кредита: " + moneyNeeded);
-                System.out.println("Машина куплена!");
-            } else {
-                System.out.println("Ожидание более 12 месяцев, но у вас нет кредитной истории.");
-                System.out.println("Придется копить.");
-            }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите день недели (число от 1 до 7): ");
+        int day = input.nextInt();
+        String dayString;
+        String dayoffOrWorkday = (day == 6 || day == 7) ? "Выходной день" : "Рабочий день";
+        switch (day) {
+            case 1: dayString = "Понедельник"; break;
+            case 2: dayString = "Вторник"; break;
+            case 3: dayString = "Среда"; break;
+            case 4: dayString = "Четверг"; break;
+            case 5: dayString = "Пятница"; break;
+            case 6: dayString = "Суббота"; break;
+            case 7: dayString = "Воскресенье"; break;
+            default:
+                dayString = "Некорректный ввод";
+                dayoffOrWorkday = "";
         }
-        scanner.close();
+
+        if (!dayoffOrWorkday.isEmpty()) {
+            System.out.println(dayString + " - " + dayoffOrWorkday);
+        } else {
+            System.out.println("Ошибка: " + dayString);
+        }
+
+        input.close();
     }
 }
-
