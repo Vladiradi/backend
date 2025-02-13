0e72118c1f7e3011e0dbe1b9f63157c2f4c5b382
Vladiradi <rzhvlad@gmail.com>
2025-02-13 22:26:02 +0100

    99%

--- Number.java
+++ Number.java
@@ -2,8 +2,30 @@
 //Проверить, делится ли число на 2 и 3 одновременно, 5 и 7 одновременно, 3 и 5  одновременно без остатка.
 //Использовать форматированный(System.out.printf) вывод для этого.
 //Продебажить
+
+import java.util.Scanner;
+
 public class Number {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        if (number % 2 == 0 && number % 3 == 0) {
+            System.out.printf("%d делится на 2 и 3 одновременно.%n", number);
+        } else {
+            System.out.printf("%d не делится на 2 и 3 одновременно.%n", number);
+        }
+        if (number % 5 == 0 && number % 7 == 0) {
+            System.out.printf("%d делится на 5 и 7 одновременно.%n", number);
+        } else {
+            System.out.printf("%d не делится на 5 и 7 одновременно.%n", number);
+        }
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.printf("%d делится на 3 и 5 одновременно.%n", number);
+        } else {
+            System.out.printf("%d не делится на 3 и 5 одновременно.%n", number);
+        }
+        input.close();
     }
 }
+
