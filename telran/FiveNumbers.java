806b88a86f1925af2afa9563a0eb4c26c436a9de
Vladiradi <rzhvlad@gmail.com>
2025-02-27 10:14:12 +0100

    Removed

---
+++ telran/FiveNumbers.java
@@ -0,0 +1,42 @@
+
+
+//№2.
+//
+//Создайте массив из 5 случайных целых чисел из интервала [10;99]
+//Выведите его на консоль в строку.
+//Определите и выведите на экран сообщение о том, является ли массив строго возрастающей последовательностью.
+
+
+package telran;
+import java.util.Random;
+
+public class FiveNumbers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(10,99);
+        }
+
+        System.out.print("Array: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        boolean isIncreasing = true;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] <= numbers[i - 1]) {
+                isIncreasing = false;
+                break;
+            }
+        }
+
+        if (isIncreasing) {
+            System.out.println("The array is a strictly increasing.");
+        } else {
+            System.out.println("The array is NOT a strictly increasing");
+        }
+    }
+}
