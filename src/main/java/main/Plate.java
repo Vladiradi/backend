a54003369a42641883dbba4a02114b7b44a29306
Vladiradi <rzhvlad@gmail.com>
2025-03-03 22:54:14 +0100

    40%

--- src/main/java/main/Plate.java
+++ src/main/java/main/Plate.java
@@ -9,6 +9,32 @@ public class Plate {
           this.serving = serving;
       }
       public void serve(){
-          System.out.println("Serving " + serving);
+          if (serving > 0 ) {
+              System.out.println("This plate is served by " + dishName);
+              serving--;
+              System.out.println("Portions left: " + serving);
+          } else {
+              System.out.println("There are no portions left");
+          }
       }
+      public String getDishName() {
+          return dishName;
+      }
+      public void setDishName(String dishName) {
+          this.dishName = dishName;
+      }
+      public int getServing() {
+          return serving;
+      }
+      public void setServing(int serving) {
+          this.serving = serving;
+      }
+
+    public static void main(String[] args) {
+        Plate[] plates = {
+                new Plate("Sushi", 10),
+                new Plate("Sake", 20),
+                new Plate("Misosoup", 30),
+        }
+    }
 }
