59b2b8de7509f257ceadf6b3d2f6d3b60e13e6a9
Vladiradi <rzhvlad@gmail.com>
2025-03-03 22:13:09 +0100

    20%

--- src/main/java/main/Plate.java
+++ src/main/java/main/Plate.java
@@ -1,5 +1,14 @@
 package main;
 
 public class Plate {
+      private String dishName;
+      private int serving;
 
+      public Plate(String dishName, int serving) {
+          this.dishName = dishName;
+          this.serving = serving;
+      }
+      public void serve(){
+          System.out.println("Serving " + serving);
+      }
 }
