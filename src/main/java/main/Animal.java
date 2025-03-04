b072681994a9d8031040487c40021db4caf76af3
Vladiradi <rzhvlad@gmail.com>
2025-03-04 12:33:30 +0100

    last task of the day

--- src/main/java/main/Animal.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-abstract class Animal {
-    protected int age;00
-    public Animal(int age) {
-        this.age = age;
-    }
-}
--- src/main/java/main/Dog.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-class Dog extends Animal {
-    public Dog(int age) {
-        super(age);
-    }
-}
