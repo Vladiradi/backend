af646e28b0f1a38a126ab46303bba3f44926cedf
Vladiradi <rzhvlad@gmail.com>
2025-03-31 11:34:39 +0200

    2

---
+++ src/main/java/main/Student.java
@@ -0,0 +1,31 @@
+package main;
+
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+
