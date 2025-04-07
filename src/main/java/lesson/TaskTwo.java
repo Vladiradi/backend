2004aaecc7533241b4df082f4384e7a02c14e5ab
Vladiradi <rzhvlad@gmail.com>
2025-04-07 23:04:05 +0200

    g

---
+++ src/main/java/lesson/TaskTwo.java
@@ -0,0 +1,20 @@
+package lesson;
+import  java.util.Arrays;
+import java.util.HashSet;
+import  java.util.List;
+
+
+
+public class TaskTwo {
+public static void main(String[] args) {
+    List<Integer> one = Arrays.asList(3,4,5,6,73,5,7,8,4,6,8);
+    List<Integer> two = Arrays.asList(5,3,5,6,7,4,7,85,4,7,9);
+
+
+
+
+
+
+}
+
+}
