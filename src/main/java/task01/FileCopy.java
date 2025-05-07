6e51dd1682e24d04f136ba354c5feea868b2de29
Vladiradi <rzhvlad@gmail.com>
2025-05-07 21:44:02 +0200

    first task 01

---
+++ src/main/java/task01/FileCopy.java
@@ -0,0 +1,31 @@
+package task01;
+import java.io.IOException;
+import java.nio.file.*
+import java.util.Scanner;
+
+public class FileCopy {
+    Scanner text = new Scanner(System.in);
+    sout
+}
+
+{
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Введите путь к существующему файлу: ");
+//        String sourcePathStr = scanner.nextLine();
+//
+//        System.out.print("Введите имя нового файла (с путем или без): ");
+//        String destPathStr = scanner.nextLine();
+//
+//        Path sourcePath = Paths.get(sourcePathStr);
+//        Path destPath = Paths.get(destPathStr);
+//
+//        try {
+//            Files.copy(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING);
+//            System.out.println("Файл успешно скопирован!");
+//        } catch (IOException e) {
+//            System.out.println("Ошибка при копировании файла: " + e.getMessage());
+//        }
+//    }
+//}
