45d99781a09c8ed36d68949b820c02d5dea07dfa
Vladiradi <rzhvlad@gmail.com>
2025-05-07 21:56:18 +0200

    first task done

--- src/main/java/task01/FileCopy.java
+++ src/main/java/task01/FileCopy.java
@@ -1,31 +1,30 @@
+//Написать приложение, которое будет копировать информацию из файла в другой файл с использованием NIO.
+//        Путь к существующему файлу и имя нового файла нужно вводить с клавиатуры.
+
 package task01;
+
 import java.io.IOException;
-import java.nio.file.*
+import java.nio.file.*;
 import java.util.Scanner;
 
 public class FileCopy {
-    Scanner text = new Scanner(System.in);
-    sout
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write path for file: ");
+        String sourcePath = scanner.nextLine();
+        System.out.println("Type name for new file with path");
+        String destPath = scanner.nextLine();
+
+        Path source = Paths.get(sourcePath);
+        Path dest = Paths.get(destPath);
+
+        try {
+            Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Done successfully");
+        } catch (IOException e) {
+            System.out.println("Error while copying " + e.getMessage());
+        }
+    }
+
 }
 
-{
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Введите путь к существующему файлу: ");
-//        String sourcePathStr = scanner.nextLine();
-//
-//        System.out.print("Введите имя нового файла (с путем или без): ");
-//        String destPathStr = scanner.nextLine();
-//
-//        Path sourcePath = Paths.get(sourcePathStr);
-//        Path destPath = Paths.get(destPathStr);
-//
-//        try {
-//            Files.copy(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING);
-//            System.out.println("Файл успешно скопирован!");
-//        } catch (IOException e) {
-//            System.out.println("Ошибка при копировании файла: " + e.getMessage());
-//        }
-//    }
-//}
