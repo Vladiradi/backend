934e59df037c364a555f18f9923686f722453d9c
Vladiradi <rzhvlad@gmail.com>
2025-06-23 21:58:52 +0200

    start second task

---
+++ src/main/java/task02/ReflectionWriter.java
@@ -0,0 +1,13 @@
+package task02;
+import java.lang.reflect.Method;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Set;
+
+
+public class ReflectionWriter {
+    public static void main(String[] args) {
+
+    }
+}
